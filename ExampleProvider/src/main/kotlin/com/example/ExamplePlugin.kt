59034a72943a738036c420b006cb03dcf406d511
Luna712 <142361265+Luna712@users.noreply.github.com>
2024-07-20 11:26:00 -0600

    Test

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -2,23 +2,13 @@ package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.APIHolder
 import android.content.Context
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
-    var activity: AppCompatActivity? = null
-
+class TwitchPlugin: Plugin() {
     override fun load(context: Context) {
-        activity = context as AppCompatActivity
-        // All providers should be added in this manner
-        registerMainAPI(ExampleProvider(this))
-
-        openSettings = { ctx ->
-            val frag = BlankFragment(this)
-            frag.show(activity!!.supportFragmentManager, "Frag")
-        }
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(ExampleProvider())
+        registerExtractorAPI(ExampleProvider.ExampleExtractor())
     }
 }
