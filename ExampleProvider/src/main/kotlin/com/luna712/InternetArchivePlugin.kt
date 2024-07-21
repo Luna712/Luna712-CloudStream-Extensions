eb5bd45a8983bf3ed32791dae4af3d99710bbd4b
Luna712 <142361265+Luna712@users.noreply.github.com>
2024-07-21 09:55:02 +0800

    Update

--- ExampleProvider/src/main/kotlin/com/luna712/InternetArchivePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/luna712/InternetArchivePlugin.kt
@@ -1,14 +1,14 @@
-package com.example
+package com.luna712
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class ExamplePlugin: Plugin() {
+class InternetArchivePlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(ExampleProvider())
-        registerExtractorAPI(ExampleProvider.ExampleExtractor())
+        registerMainAPI(InternetArchiveProvider())
+        registerExtractorAPI(InternetArchiveProvider.InternetArchiveExtractor())
     }
 }
