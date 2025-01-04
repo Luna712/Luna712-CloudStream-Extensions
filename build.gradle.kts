cad04773b8e66184723e41c4e104b66bdf165af1
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-04 12:15:50 -0700

    2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ subprojects {
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
-        implementation(kotlin("stdlib")) // adds standard kotlin features
+        implementation(kotlin("stdlib", "2.1.0")) // adds standard kotlin features
         implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
         implementation("org.jsoup:jsoup:1.18.3") // html parser
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // json parser
