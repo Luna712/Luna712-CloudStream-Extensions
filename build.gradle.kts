0c7d98912356ab6765e4e3062fffb47f63bac78d
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-29 19:08:37 -0700

    Revert

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         // CloudStream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.Luna712:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
