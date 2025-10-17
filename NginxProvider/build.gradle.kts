3924836af5de6a68d5b2d3811661cc710f61fa19
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-17 12:25:53 -0600

    Add NginxProvider

---
+++ NginxProvider/build.gradle.kts
@@ -0,0 +1,30 @@
+dependencies {
+    implementation("com.google.android.material:material:1.14.0-alpha05")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.9.4")
+}
+
+// Use an integer for version numbers
+version = 1
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+    // authors = listOf("Cloudburst")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "TvSeries",
+        "Movie",
+    )
+    requiresResources = true
+}
