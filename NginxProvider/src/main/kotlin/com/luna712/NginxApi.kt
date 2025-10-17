641c90e51d07ca554dffab0c1fc405a9f88b43a1
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-17 13:47:51 -0600

    New API

--- NginxProvider/src/main/kotlin/com/luna712/NginxApi.kt
+++ NginxProvider/src/main/kotlin/com/luna712/NginxApi.kt
@@ -3,25 +3,27 @@
  import com.lagradost.cloudstream3.AcraApplication.Companion.getKey
  import com.lagradost.cloudstream3.AcraApplication.Companion.setKey
  import com.lagradost.cloudstream3.R
- import com.lagradost.cloudstream3.syncproviders.AccountManager
  import com.lagradost.cloudstream3.syncproviders.AuthAPI
- import com.lagradost.cloudstream3.syncproviders.InAppAuthAPI
- import com.lagradost.cloudstream3.syncproviders.InAppAuthAPIManager
 
- class NginxApi(index: Int) : InAppAuthAPIManager(index) {
+import com.lagradost.cloudstream3.syncproviders.SyncAPI
+import com.lagradost.cloudstream3.syncproviders.AuthLoginRequirement
+
+ class NginxApi : SyncAPI() {
      override val name = "Nginx"
      override val idPrefix = "nginx"
      override val icon = R.drawable.ic_baseline_extension_24
-     override val requiresUsername = true
-     override val requiresPassword = true
-     override val requiresServer = true
      override val createAccountUrl = "https://www.cloudstream.cf/docs/users/use-nginx.md"
+     override val inAppLoginRequirement = AuthLoginRequirement(
+         password = true,
+         username = true,
+         server = true,
+    )
 
      companion object {
          const val NGINX_USER_KEY: String = "nginx_user"
      }
 
-     override fun getLatestLoginData(): InAppAuthAPI.LoginData? {
+     private fun getLatestLoginData(): AuthLoginResponse? {
          return getKey(accountId, NGINX_USER_KEY)
      }
 
@@ -30,20 +32,14 @@
          return AuthAPI.LoginInfo(name = data.username ?: data.server, accountIndex = accountIndex)
      }
 
-     override suspend fun login(data: InAppAuthAPI.LoginData): Boolean {
-         if (data.server.isNullOrBlank()) return false // we require a server
+     override suspend fun login(form: AuthLoginResponse): AuthToken? {
+         if (form.server.isNullOrBlank()) return null // we require a server
          switchToNewAccount()
-         setKey(accountId, NGINX_USER_KEY, data)
+         setKey(accountId, NGINX_USER_KEY, form)
          registerAccount()
-         initialize()
-         AccountManager.inAppAuths
-
-         return true
-     }
-
-     override fun logOut() {
-         removeAccountKeys()
          initializeData()
+
+         return AuthToken(payload = form.toJson())
      }
 
      private fun initializeData() {
@@ -54,9 +50,5 @@
          }
          NginxProvider.overrideUrl = data.server?.removeSuffix("/") + "/"
          NginxProvider.loginCredentials = "${data.username ?: ""}:${data.password ?: ""}"
-     }
-
-     override suspend fun initialize() {
-         initializeData()
      }
  }
