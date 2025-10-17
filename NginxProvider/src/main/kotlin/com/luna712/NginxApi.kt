34b002166036ebc7f7cca12201a4f8088b019ecd
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-17 14:10:11 -0600

    Try

--- NginxProvider/src/main/kotlin/com/luna712/NginxApi.kt
+++ NginxProvider/src/main/kotlin/com/luna712/NginxApi.kt
@@ -5,9 +5,12 @@ import com.lagradost.cloudstream3.AcraApplication.Companion.setKey
 import com.lagradost.cloudstream3.R
 import com.lagradost.cloudstream3.syncproviders.AuthAPI
 
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import com.lagradost.cloudstream3.syncproviders.SyncRepo
 import com.lagradost.cloudstream3.syncproviders.SyncAPI
 import com.lagradost.cloudstream3.syncproviders.AuthLoginResponse
 import com.lagradost.cloudstream3.syncproviders.AuthToken
+import com.lagradost.cloudstream3.syncproviders.AuthUser
 import com.lagradost.cloudstream3.syncproviders.AuthLoginRequirement
 
  class NginxApi : SyncAPI() {
@@ -29,16 +32,16 @@ import com.lagradost.cloudstream3.syncproviders.AuthLoginRequirement
          return getKey(accountId, NGINX_USER_KEY)
      }
 
-     override fun loginInfo(): AuthAPI.LoginInfo? {
+     override fun user(token: AuthToken?): AuthUser? {
          val data = getLatestLoginData() ?: return null
-         return AuthAPI.LoginInfo(name = data.username ?: data.server, accountIndex = accountIndex)
+         return AuthUser(name = data.username ?: data.server, id = SyncRepo(this).accountId)
      }
 
      override suspend fun login(form: AuthLoginResponse): AuthToken? {
          if (form.server.isNullOrBlank()) return null // we require a server
          switchToNewAccount()
-         setKey(accountId, NGINX_USER_KEY, form)
-         registerAccount()
+         setKey(SyncRepo(this).accountId, NGINX_USER_KEY, form)
+         // registerAccount()
          initializeData()
 
          return AuthToken(payload = form.toJson())
