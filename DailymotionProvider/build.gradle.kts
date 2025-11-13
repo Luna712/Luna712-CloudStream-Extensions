a542b620b0c085bf04ab63b43d255a2b15fb5b3a
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-12 19:00:24 -0700

    isCrossPlatform

--- DailymotionProvider/build.gradle.kts
+++ DailymotionProvider/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
     tvTypes = listOf("Others")
     iconUrl = "https://www.google.com/s2/favicons?domain=www.dailymotion.com&sz=%size%"
 
-    isCrossPlatform = false
+    isCrossPlatform = true
 }
